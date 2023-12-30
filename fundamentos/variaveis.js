40e8a568bf69d3178a4828ba099500d8c2f5b9f8
BeHaupt <bersurf@outlook.com>
2023-12-30 16:33:55 -0300

    Add Ex001

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,15 @@
+//var
+//let
+//const
+
+//Abaixo os 3 tipos de VARIAVEIS.
+var nome = "Bernardo";
+let ano = 2004;
+const possuiFaculdade = false;
+
+//Abaixo var com mais de um dado.
+var nome = "Bernardo",
+  idade = 19,
+  PossuiExp = true;
+
+console.log("Dados do Usuario:" + nome + ",", idade + " anos,", PossuiExp);
--- variaveis.js
+++
@@ -1,9 +0,0 @@
-//var
-//let
-//const
-
-var nome = "Bernardo",
-  idade = 19,
-  PossuiExp = true;
-
-  console.log(nome, idade, PossuiExp)
